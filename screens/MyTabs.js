c5d1b39b24649d8b2a4c37b95833c83f506bd76c
Nihat <nihatpmkc@gmail.com>
2021-02-03 21:48:23 +0300

    x

--- screens/MyTabs.js
+++ screens/MyTabs.js
@@ -3,34 +3,54 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './HomeScreen';
 import SettingsScreen from './SettingsScreen';
 import ProfileScreen from './ProfileScreen';
-const Tab = createBottomTabNavigator();
+
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Tab = createBottomTabNavigator();
+
 function MyTabs() {
   return (
     <NavigationContainer>
 
       <Tab.Navigator>
+
+
+
       <Tab.Screen name="Home"  component={HomeScreen} options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={'green'} size={40} />
           ),
         }} />
+
+
+
+
       <Tab.Screen name="Settings" component={SettingsScreen}  options={{
           tabBarLabel: 'Emergency',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="sign-caution" color={'red'} size={40} />
           ),
         }}/>
+
+
+
+
+
       <Tab.Screen name="ProfileScreen" component={ProfileScreen}  options={{
           tabBarLabel: 'Profile',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="face-profile" color={'blue'} size={37} />
           ),
         }} />
+
+
+
+
       </Tab.Navigator>
     </NavigationContainer>
       
